8b093ac29f8cfcde8f9c7965c05901741e1f59db
Pavle Josić <pavlejosic2004@gmail.com>
2026-04-26 10:49:14 +0200

    minor changes

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,52 +1,11 @@
 import { Injectable, signal } from '@angular/core';
-import { User } from '../../db/users.db';
+import { User, users } from '../../db/users.db';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private users: User[] = [
-    {
-      id: 1,
-      ime: "Petar",
-      prezime: "Petrovic",
-      email: "petar@petrovic.com",
-      telefon: "+381098765432",
-      adresa: "CBA 321",
-      omiljeneVrsteIgracaka: ["Slagalica", "Slikovnica", "Figura", "Kreativni set", "Vozilo", "Plišana igračka", "Društvena igra", "Konstruktorski set"],
-      password: "petar123"
-    },
-    {
-      id: 2,
-      ime: "Jovan",
-      prezime: "Jovanovic",
-      email: "jovan@jovanovic.com",
-      telefon: "+381069555333",
-      adresa: "AAA 111",
-      omiljeneVrsteIgracaka: ["Slagalica", "Slikovnica", "Figura", "Kreativni set", "Vozilo", "Plišana igračka", "Društvena igra", "Konstruktorski set"],
-      password: "jovan123"
-    },
-    {
-      id: 3,
-      ime: "Marko",
-      prezime: "Markovic",
-      email: "marko@markovic.com",
-      telefon: "+38169123456",
-      adresa: "BBB 222",
-      omiljeneVrsteIgracaka: ["Slagalica", "Slikovnica", "Figura", "Kreativni set", "Vozilo", "Plišana igračka", "Društvena igra", "Konstruktorski set"],
-      password: "marko123"
-    },
-    {
-      id: 4,
-      ime: "Stefan",
-      prezime: "Stefanovic",
-      email: "stefan@stefanovic.com",
-      telefon: "+38169654321",
-      adresa: "CCC 333",
-      omiljeneVrsteIgracaka: ["Slagalica", "Slikovnica", "Figura", "Kreativni set", "Vozilo", "Plišana igračka", "Društvena igra", "Konstruktorski set"],
-      password: "stefan123"
-    }
-  ];
+  private users: User[] = users;
   
   public trenutniKorisnik = signal<User | null>(null);
 
